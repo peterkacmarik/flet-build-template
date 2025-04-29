4483b8a68d327259013bbdb94afd86762575ec61
Peter Kačmarík <124926474+peterkacmarik@users.noreply.github.com>
2025-04-29 16:44:46 +0200

    Delete {{cookiecutter.out_dir}}/android/app/src/main/kotlin/{{ cookiecutter.kotlin_dir }}/Utils.kt

--- {{cookiecutter.out_dir}}/android/app/src/main/kotlin/{{ cookiecutter.kotlin_dir }}/Utils.kt
+++
@@ -1,6 +0,0 @@
-package com.example.workation
-
-fun formatTime(hours: String, minutes: String, seconds: String) =
-    "${hours}:${minutes}:${seconds}"
-
-fun Int.pad() = toString().padStart(2, '0')
