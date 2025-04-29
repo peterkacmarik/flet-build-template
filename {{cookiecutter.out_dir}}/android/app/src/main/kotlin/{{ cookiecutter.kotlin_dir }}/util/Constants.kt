2c57019ec1b91363d4f690e903d03d951ac356bc
Peter Kačmarík <124926474+peterkacmarik@users.noreply.github.com>
2025-04-29 17:21:27 +0200

    Update Constants.kt

--- {{cookiecutter.out_dir}}/android/app/src/main/kotlin/{{ cookiecutter.kotlin_dir }}/util/Constants.kt
+++ {{cookiecutter.out_dir}}/android/app/src/main/kotlin/{{ cookiecutter.kotlin_dir }}/util/Constants.kt
@@ -1,10 +1,18 @@
+// file: com/example/workation/util/Constants.kt
 package com.example.workation.util
 
 object Constants {
     const val ACTION_SERVICE_START        = "ACTION_SERVICE_START"
     const val ACTION_SERVICE_STOP         = "ACTION_SERVICE_STOP"
     const val ACTION_SERVICE_CANCEL       = "ACTION_SERVICE_CANCEL"
+
     const val NOTIFICATION_CHANNEL_ID     = "STOPWATCH_CHANNEL"
     const val NOTIFICATION_CHANNEL_NAME   = "Stopwatch Channel"
     const val NOTIFICATION_ID             = 1001
+
+    // request codes pre PendingIntent
+    const val CLICK_REQUEST_CODE   = 100
+    const val CANCEL_REQUEST_CODE  = 101
+    const val STOP_REQUEST_CODE    = 102
+    const val RESUME_REQUEST_CODE  = 103
 }
