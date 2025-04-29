2f7bb60793809708bf7633b856e1ad840bbd7d15
Peter Kačmarík <124926474+peterkacmarik@users.noreply.github.com>
2025-04-29 16:25:35 +0200

    Create NotificationModule.kt

---
+++ {{cookiecutter.out_dir}}/android/app/src/main/kotlin/{{ cookiecutter.kotlin_dir }}/NotificationModule.kt
@@ -0,0 +1,40 @@
+package com.example.workation
+
+import android.app.NotificationManager
+import android.content.Context
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.core.app.NotificationCompat
+import com.example.stopwatch.util.Constants.NOTIFICATION_CHANNEL_ID
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+
+@ExperimentalAnimationApi
+@Module
+@InstallIn(ServiceComponent::class)
+object NotificationModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideNotificationManager(
+        @ApplicationContext context: Context
+    ): NotificationManager = context.getSystemService(
+        Context.NOTIFICATION_SERVICE
+    ) as NotificationManager
+
+    @ServiceScoped
+    @Provides
+    fun provideNotificationBuilder(
+        @ApplicationContext context: Context
+    ): NotificationCompat.Builder = NotificationCompat.Builder(
+        context, NOTIFICATION_CHANNEL_ID
+    )
+        .setContentTitle("Stopwatch")
+        .setContentText("00:00:00")
+        .setSmallIcon(android.R.drawable.ic_media_play)
+        .setOngoing(true)
+        .setContentIntent(ServiceHelper.clickPendingIntent(context))
+}
